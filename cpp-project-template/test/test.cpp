7f6b64084dede56af67c916d329f1fcabd6775cb
Michael Tsukerman <miketsukerman@gmail.com>
2021-06-11 23:12:45 +0200

    cpp: common C++ project

---
+++ cpp-project-template/test/test.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "sample.h"
+
+SCENARIO("testcase", "")
+{
+    GIVEN("usercase 2+2")
+    {
+        SampleObject sm;
+
+        WHEN("instance is created")
+        {
+            THEN("test sum(2,2)")
+            {
+                REQUIRE(sm.sum(2,2) == 4);
+            }
+        }
+    }
+}
